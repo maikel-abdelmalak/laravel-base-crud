51254ba7d5fec939225de10c6dd6f4617a32b880
maikel abdelmalak <maikel22@hotmail.it>
2020-07-09 20:48:33 +0200

    active route

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,11 +3,11 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
+      <li class="nav-item {{Request::Route()->GetName()=='home'?'active':''}}">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">STUDENTS</a>
+      <li class="nav-item {{strpos(Request::route()->getName(),'students') !== false ? 'active' : ''}}">
+        <a class="nav-link" href="#">Students</a>
       </li>
 
     </ul>
